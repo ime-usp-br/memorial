c6666286210ed4f50b677749f6c4884a3d32eb76
Marcelo Modesto Costa <marcelomodesto@gmail.com>
2021-10-27 14:09:43 -0300

    mudança no botão de remover curador

--- resources/views/users/curadores/partials/curador.blade.php
+++ resources/views/users/curadores/partials/curador.blade.php
@@ -1,4 +1,4 @@
 <strong>{{ $curador->name }}</strong>
 @if(Auth::user()->id != $curador->id)
-<a class="btn btn-outline-dark" href="/admin/removercurador/{{$curador->id}}/{{$homenageado->id}}" onclick="return confirm('Tem certeza?');">Remover</a>
+[ <a class="link-dark text-decoration-none" href="/admin/removercurador/{{$curador->id}}/{{$homenageado->id}}" onclick="return confirm('Tem certeza?');">remover</a> ]
 @endif
