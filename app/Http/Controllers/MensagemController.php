978ca5ad864e85511ed9026dce1ec36037b7b23a
GustavoSantos25 <gustavo.war2015@gmail.com>
2021-11-03 18:11:24 -0300

    Colocando a mensagem como APROVADA caso seja adicionada por admins ou curadores

--- app/Http/Controllers/MensagemController.php
+++ app/Http/Controllers/MensagemController.php
@@ -53,7 +53,14 @@ class MensagemController extends Controller
         $msg = Mensagem::create($validated);
         $user = new User;
         $homenageado = Homenageado::find($msg->homenageado_id);
+
+        //apenas manda email se não for curador ou admin
+        //c.c, já coloca a mensagem como aprovada
         if(!Gate::allows('administrador') && !Gate::allows('curador', [$msg->homenageado_id])) Mail::send(new mensagemPendente($msg, $homenageado, $user->admins(),$homenageado->curadores()));
+        else{
+            $msg->estado = "APROVADO";
+            $msg->save();
+        }
         request()->session()->flash('alert-info', 'Mensagem aguardando validação');
         return redirect("/homenageados/$msg->homenageado_id");
     }
