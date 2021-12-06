e3467bc99cb7f2849690640df5a1eb4004131e13
marcelomodesto <marcelomodesto@gmail.com>
2021-12-06 09:40:54 -0300

    alterando estilo dos bullets

--- resources/views/mensagens/partials/fields.blade.php
+++ resources/views/mensagens/partials/fields.blade.php
@@ -1,64 +1,55 @@
 <div class="card">
 
-      <div class="card-body">
-            
-            <p class="card-text">{{$mensagem->mensagem}}</p>
-            <p class="text-muted"><i>{{$mensagem->nome}}</i> <br> {{$mensagem->instituicao}}</p>
-            @if(Gate::allows('administrador') || Gate::allows('curador', [$mensagem->homenageado_id]))
-            <div>
-                  <button class="btn btn-outline-dark child" data-bs-toggle="modal" data-bs-target="#edit-{{$mensagem->id}}">Editar</button>
+    <div class="card-body">
 
-                  <div class="modal fade" id="edit-{{$mensagem->id}}" tabindex="-1" role="dialog">
-                  <div class="modal-dialog" role="document">
+        <p class="card-text">{{ $mensagem->mensagem }}</p>
+        <p class="text-muted"><i>{{ $mensagem->nome }}</i> <br> {{ $mensagem->instituicao }}</p>
+        @if (Gate::allows('administrador') || Gate::allows('curador', [$mensagem->homenageado_id]))
+            <div>
+                <button class="btn btn-outline-dark child" data-bs-toggle="modal"
+                    data-bs-target="#edit-{{ $mensagem->id }}">Editar</button>
+
+
+                    @if ($mensagem->estado == 'APROVADO')
+                        <span class="text-success p-3">&#9679;</span>
+                    @elseif($mensagem->estado == "PENDENTE")
+                    <span class="text-danger p-3">&#9679;</span>
+                    @elseif($mensagem->estado == "NEGADO")
+                    <span class="text-warning p-3">&#9679;</span>
+                @endif
+                    
+
+                <div class="modal fade" id="edit-{{ $mensagem->id }}" tabindex="-1" role="dialog">
+                    <div class="modal-dialog" role="document">
                         <div class="modal-content">
 
-                        <div class="modal-header">
-                        <button type="button" data-bs-dismiss="modal" class="btn-close" aria-label="Close"></button>
+                            <div class="modal-header">
+                                <button type="button" data-bs-dismiss="modal" class="btn-close"
+                                    aria-label="Close"></button>
+                            </div>
+
+
+                            <?php
+                            $homenageado_id = $mensagem->homenageado_id;
+                            $edit = true;
+                            ?>
+
+
+                            <div class="modal-body">
+
+                                @include('mensagens.edit')
+
+
+                                
                         </div>
-                        
 
-                        <?php
-                        $homenageado_id = $mensagem->homenageado_id;
-                        $edit = true;
-                        ?>
+                    </div>
+                </div>
 
-                        
-                        <div class="modal-body">
+               
 
-                              @include('mensagens.edit')
-                              
-                        </div>
-                        
-
-                        </div>
-
-                  </div>
-                  </div>
-                  
-                  <div class="child">
-                  @if($mensagem->estado == "APROVADO")
-                  <div class="bulletpoint-aprovado" >
-                        <li></li>
-                  </div>
-                  @elseif($mensagem->estado == "PENDENTE")
-                  <div class="bulletpoint-pendente"> 
-                        <li></li>
-                  </div>
-                  @elseif($mensagem->estado == "NEGADO")
-                  <div class="bulletpoint-negado" >
-                        <li></li>
-                  </div>
-                  @endif
-                  </div>
- 
             </div>
-            
-            @endif  
-      </div>
+
+        @endif
+    </div>
 </div>
-
-
-
-
-
-
