de61dcc901a5c56dfd69ddcf7e1c6a91694ffcab
GustavoSantos25 <gustavo.war2015@gmail.com>
2021-12-06 10:35:15 -0300

    Adicionando link para o captcha na main

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -11,7 +11,9 @@
     <link rel="stylesheet" href="https://unpkg.com/@coreui/icons@2.0.0-beta.3/css/all.min.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Cinzel:wght@400&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Cinzel:wght@400&display=swap" 
+    rel="stylesheet">
+    <link href="{{ captcha_layout_stylesheet_url() }}" type="text/css" rel="stylesheet">
 </head>
 
 <body>
