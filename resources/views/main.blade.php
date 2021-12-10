bc7c26ea34bd9d6a6d958549c6421111e6e49045
GustavoSantos25 <gustavo.war2015@gmail.com>
2021-12-10 15:23:40 -0300

    Removendo captcha + retornando forms de mensagem para o modal

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -13,7 +13,6 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Cinzel:wght@400&display=swap" 
     rel="stylesheet">
-    <link href="{{ captcha_layout_stylesheet_url() }}" type="text/css" rel="stylesheet">
 </head>
 
 <body>
